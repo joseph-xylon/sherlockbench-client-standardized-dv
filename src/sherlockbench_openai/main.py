23a6e04bf1a28366bbb5eaf6926a95cdfcba69be
Joseph Graham <joseph@xylon.me.uk>
2024-12-25 16:27:50 +0000

    perform the tool_calls

--- src/sherlockbench_openai/main.py
+++ src/sherlockbench_openai/main.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import yaml
+import json
 import requests
 from operator import itemgetter
 from .prompts import initial_messages
@@ -24,10 +25,10 @@ def get(url, params=None):
     response.raise_for_status()  # Raise an error for HTTP issues
     return response.json()
 
-def post(base_url, run_id, path, data=None, headers=None):
+def post(base_url, run_id, path, data):
     data["run-id"] = run_id
 
-    response = requests.post(base_url + path, json=data, headers=headers)
+    response = requests.post(base_url + path, json=data)
     response.raise_for_status()
     return response.json()
 
@@ -41,6 +42,28 @@ def create_completion(client, model, **kwargs):
         model=model,
         **kwargs
     )
+
+def normalize_args(input_dict):
+    """Converts a dict into a list of values, sorted by the alphabetical order of the keys."""
+    return [input_dict[key] for key in sorted(input_dict.keys())]
+
+def handle_tool_call(postfn, attempt_id, call):
+    arguments = json.loads(call.function.arguments)
+    args_norm = normalize_args(arguments)
+
+    fnoutput = postfn("test-function", {"attempt-id": attempt_id,
+                                        "args": args_norm})
+
+    print(args_norm)
+    print(fnoutput)
+
+    function_call_result_message = {
+        "role": "tool",
+        "content": json.dumps(fnoutput),
+        "tool_call_id": call.id
+    }
+
+    return function_call_result_message
 
 def interrogate_and_verify(postfn, completionfn, attempt_id, arg_spec):
     tools = [
@@ -57,12 +80,33 @@ def interrogate_and_verify(postfn, completionfn, attempt_id, arg_spec):
         }
     ]
 
-    completion = completionfn(
-        messages=initial_messages,
-        tools=tools
-    )
+    # call the LLM repeatedly until it stops calling it's tool
+    messages=initial_messages
+    while True:
+        completion = completionfn(
+            messages=messages,
+            tools=tools
+        )
 
-    print(completion.choices[0].message.tool_calls)
+        response = completion.choices[0]
+        message = response.message.content
+        tool_calls = response.message.tool_calls
+
+        messages.append({"role": "assistant",
+                         "content": message,
+                         "tool_calls": tool_calls})
+
+        print(message)
+        print()
+
+        # if it didn't call the tool we can move on
+        if not tool_calls:
+            break
+
+        for call in tool_calls:
+            messages.append(handle_tool_call(postfn, attempt_id, call))
+
+    # now it's time for verifications
 
 def main():
     config_non_sensitive = load_config("resources/config.yaml")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from sherlockbench_openai.main import list_to_map, destructure
+from sherlockbench_openai.main import list_to_map, destructure, normalize_args
 
 def test_destructure():
     data = {'a': 1, 'b': 2, 'c': 3}
@@ -10,3 +10,6 @@ def test_list_to_map():
     assert list_to_map(["integer", "integer"]) == {'a': {'type': 'integer'}, 'b': {'type': 'integer'}}
     assert list_to_map(["boolean", "boolean"]) == {'a': {'type': 'boolean'}, 'b': {'type': 'boolean'}}
     assert list_to_map(["string"]) == {'a': {'type': 'string'}}
+
+def test_normalize_args():
+    assert normalize_args({'a': 1, 'b': 2, 'c': 3}) == [1, 2, 3]
