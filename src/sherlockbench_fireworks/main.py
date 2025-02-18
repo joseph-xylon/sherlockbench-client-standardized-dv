c8db36302d2def9f0073d6c0374e3b95149e75de
Joseph Graham <joseph@xylon.me.uk>
2025-02-18 21:16:21 +0000

    Add support for fireworks.ai

---
+++ src/sherlockbench_fireworks/main.py
@@ -0,0 +1,59 @@
+from openai import OpenAI, LengthFinishReasonError
+from sherlockbench_client import destructure, post, AccumulatingPrinter, LLMRateLimiter, q, start_run, complete_run
+
+from .prompts import initial_messages
+from .investigate import investigate
+from .verify import verify
+
+from datetime import datetime
+import psycopg2
+
+msg_limit = 50
+
+def create_completion(client, model, **kwargs):
+    """closure to pre-load the model"""
+    return client.chat.completions.create(
+        model=model,
+        **kwargs
+    )
+
+def investigate_and_verify(postfn, completionfn, config, attempt_id, arg_spec, run_id, cursor):
+    start_time = datetime.now()
+    start_api_calls = completionfn.total_call_count
+
+    # setup the printer
+    printer = AccumulatingPrinter()
+
+    printer.print("\n### SYSTEM: interrogating function with args", arg_spec)
+
+    messages = initial_messages.copy()
+    messages, tool_call_count = investigate(config, postfn, completionfn, messages, printer, attempt_id, arg_spec)
+
+    printer.print("\n### SYSTEM: verifying function with args", arg_spec)
+    verification_result = verify(config, postfn, completionfn, messages, printer, attempt_id)
+
+    time_taken = (datetime.now() - start_time).total_seconds()
+    q.add_attempt(cursor, run_id, verification_result, time_taken, tool_call_count, printer, completionfn, start_api_calls, attempt_id)
+
+    return verification_result
+
+def main():
+    config, db_conn, cursor, run_id, attempts, start_time = start_run("fireworks")
+
+    client = OpenAI(base_url="https://api.fireworks.ai/inference/v1",
+                    api_key=config['api-keys']['fireworks'])
+
+    postfn = lambda *args: post(config["base-url"], run_id, *args)
+    completionfn = lambda **kwargs: create_completion(client, config['model'], **kwargs)
+
+    completionfn = LLMRateLimiter(rate_limit_seconds=config['rate-limit'],
+                                  llmfn=completionfn,
+                                  backoff_exceptions=())
+
+    for attempt in attempts:
+        investigate_and_verify(postfn, completionfn, config, attempt["attempt-id"], attempt["fn-args"], run_id, cursor)
+
+    complete_run(postfn, db_conn, cursor, run_id, start_time, completionfn.total_call_count, config)
+
+if __name__ == "__main__":
+    main()
---
+++ src/sherlockbench_fireworks/verify.py
@@ -0,0 +1,51 @@
+import json
+from openai import LengthFinishReasonError
+from pydantic import BaseModel
+from .prompts import make_verification_message
+from sherlockbench_client import destructure, make_schema
+
+def verify(config, postfn, completionfn, messages, printer, attempt_id):
+    # for each verification
+    while (v_data := postfn("next-verification", {"attempt-id": attempt_id})):
+        verification = v_data["next-verification"]
+        output_type = v_data["output-type"]
+
+        printer.print("\n### SYSTEM: inputs:")
+        printer.indented_print(verification)
+
+        vmessages = messages + [make_verification_message(verification)]
+
+        try:
+            completion = completionfn(messages=vmessages,
+                                      response_format={"type": "json_object",
+                                                       "schema": make_schema(output_type).model_json_schema()})
+        except LengthFinishReasonError as e:
+            print("Caught a LengthFinishReasonError!")
+            print("Completion:", e.completion)
+
+            # well it failed so we break
+            break
+
+        response = completion.choices[0]
+
+        thoughts, expected_output = destructure(json.loads(response.message.content), "thoughts", "expected_output")
+
+        printer.print("\n--- LLM ---")
+        printer.indented_print(thoughts, "\n")
+        printer.print()
+        printer.indented_print("`" + str(expected_output) + "`\n")
+
+        vstatus = postfn("attempt-verification", {"attempt-id": attempt_id,
+                                                  "prediction": expected_output})["status"]
+
+        if vstatus in ("wrong"):
+            printer.print("\n### SYSTEM: WRONG")
+            return False
+
+        if vstatus in ("done"):
+            break
+
+        printer.print("\n### SYSTEM: CORRECT")
+
+    # if we got here all the verifications passed
+    return True
